5a592ac4765912b0bb26dcef78ac54949a0d3cc4
gkgo <91817417+gkgo@users.noreply.github.com>
2022-04-21 11:31:25 +0800

    Update r101_3x_flip_softnms.py

--- configs/albu_example/r101_3x_flip_softnms.py
+++ configs/albu_example/r101_3x_flip_softnms.py
@@ -240,7 +240,7 @@ train_pipeline = [
         type='Resize',
         # img_scale=(1333, 800),
         img_scale=[(800, 800),(1200,1200),(1600,1600)],
-        keep_ratio=True),
+        keep_ratio=False),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(
         type='Normalize',
@@ -259,7 +259,7 @@ test_pipeline = [
         img_scale=[(800, 800),(1200,1200),(1600,1600)],
         flip=True,
         transforms=[
-            dict(type='Resize', keep_ratio=True),
+            dict(type='Resize', keep_ratio=False),
             dict(type='RandomFlip'),
             dict(
                 type='Normalize',
