e5a20ae2902265d646ca9d0c665f259e53119484
gkgo <91817417+gkgo@users.noreply.github.com>
2022-04-24 15:25:38 +0800

    Update cascade_rcnn_r101_caffe_fpn_1x_coco.py

--- configs/cascade_rcnn/cascade_rcnn_r101_caffe_fpn_1x_coco.py
+++ configs/cascade_rcnn/cascade_rcnn_r101_caffe_fpn_1x_coco.py
@@ -195,44 +195,44 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(
-        type='Albu',
-        transforms=[
-            dict(
-                type='ShiftScaleRotate',
-                shift_limit=0.0625,
-                scale_limit=0.0,
-                rotate_limit=180,
-                interpolation=1,
-                p=0.5),
-            # dict(
-            #     type='RandomBrightnessContrast',
-            #     brightness_limit=[0.1, 0.3],
-            #     contrast_limit=[0.1, 0.3],
-            #     p=0.2),
-            # dict(
-            #     type='RandomBrightnessContrast',
-            #     brightness_limit=[0.1, 0.3],
-            #     contrast_limit=[0.1, 0.3],
-            #     p=0.2),
-            # dict(type='ChannelShuffle', p=0.1),
-            # dict(
-            #     type='OneOf',
-            #     transforms=[
-            #         dict(type='Blur', blur_limit=3, p=1.0),
-            #         dict(type='MedianBlur', blur_limit=3, p=1.0)
-            #     ],
-            #     p=0.1)
-        ],
-        bbox_params=dict(
-            type='BboxParams',
-            format='pascal_voc',
-            label_fields=['gt_labels'],
-            min_visibility=0.0,
-            filter_lost_elements=True),
-        keymap=dict(img='image', gt_bboxes='bboxes'),
-        update_pad_shape=False,
-        skip_img_without_anno=True),
+    # dict(
+    #     type='Albu',
+    #     transforms=[
+    #         dict(
+    #             type='ShiftScaleRotate',
+    #             shift_limit=0.0625,
+    #             scale_limit=0.0,
+    #             rotate_limit=180,
+    #             interpolation=1,
+    #             p=0.5),
+    #         # dict(
+    #         #     type='RandomBrightnessContrast',
+    #         #     brightness_limit=[0.1, 0.3],
+    #         #     contrast_limit=[0.1, 0.3],
+    #         #     p=0.2),
+    #         # dict(
+    #         #     type='RandomBrightnessContrast',
+    #         #     brightness_limit=[0.1, 0.3],
+    #         #     contrast_limit=[0.1, 0.3],
+    #         #     p=0.2),
+    #         # dict(type='ChannelShuffle', p=0.1),
+    #         # dict(
+    #         #     type='OneOf',
+    #         #     transforms=[
+    #         #         dict(type='Blur', blur_limit=3, p=1.0),
+    #         #         dict(type='MedianBlur', blur_limit=3, p=1.0)
+    #         #     ],
+    #         #     p=0.1)
+    #     ],
+    #     bbox_params=dict(
+    #         type='BboxParams',
+    #         format='pascal_voc',
+    #         label_fields=['gt_labels'],
+    #         min_visibility=0.0,
+    #         filter_lost_elements=True),
+    #     keymap=dict(img='image', gt_bboxes='bboxes'),
+    #     update_pad_shape=False,
+    #     skip_img_without_anno=True),
     dict(type='GtBoxBasedCrop', crop_size=(640,416)),
     # dict(type='Resize', img_scale=[(1600, 1064), (800, 532)], keep_ratio=True),
     dict(type='Resize', img_scale=[(1280,832), (640,416)], keep_ratio=True),
@@ -308,32 +308,32 @@ resume_from = None
 workflow = [('train', 1)]
 opencv_num_threads = 0
 mp_start_method = 'fork'
-albu_train_transforms = [
-    dict(
-        type='ShiftScaleRotate',
-        shift_limit=0.0625,
-        scale_limit=0.0,
-        rotate_limit=180,
-        interpolation=1,
-        p=0.5),
-    # dict(
-    #     type='RandomBrightnessContrast',
-    #     brightness_limit=[0.1, 0.3],
-    #     contrast_limit=[0.1, 0.3],
-    #     p=0.2),
-    # dict(
-    #     type='RandomBrightnessContrast',
-    #     brightness_limit=[0.1, 0.3],
-    #     contrast_limit=[0.1, 0.3],
-    #     p=0.2),
-    # dict(type='ChannelShuffle', p=0.1),
-    # dict(
-    #     type='OneOf',
-    #     transforms=[
-    #         dict(type='Blur', blur_limit=3, p=1.0),
-    #         dict(type='MedianBlur', blur_limit=3, p=1.0)
-    #     ],
-    #     p=0.1)
-]
+# albu_train_transforms = [
+#     dict(
+#         type='ShiftScaleRotate',
+#         shift_limit=0.0625,
+#         scale_limit=0.0,
+#         rotate_limit=180,
+#         interpolation=1,
+#         p=0.5),
+#     # dict(
+#     #     type='RandomBrightnessContrast',
+#     #     brightness_limit=[0.1, 0.3],
+#     #     contrast_limit=[0.1, 0.3],
+#     #     p=0.2),
+#     # dict(
+#     #     type='RandomBrightnessContrast',
+#     #     brightness_limit=[0.1, 0.3],
+#     #     contrast_limit=[0.1, 0.3],
+#     #     p=0.2),
+#     # dict(type='ChannelShuffle', p=0.1),
+#     # dict(
+#     #     type='OneOf',
+#     #     transforms=[
+#     #         dict(type='Blur', blur_limit=3, p=1.0),
+#     #         dict(type='MedianBlur', blur_limit=3, p=1.0)
+#     #     ],
+#     #     p=0.1)
+# ]
 auto_resume = False
 gpu_ids = [0]
